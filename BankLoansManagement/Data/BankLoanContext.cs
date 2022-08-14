762a0a69c25608f1d46e77ca642426c965354162
Proficient Mkansi <de.prof.codes@gmail.com>
2022-08-14 21:21:42 +0200

    - add database - add migration scripts for Db Init

---
+++ BankLoansManagement/Data/BankLoanContext.cs
@@ -0,0 +1,15 @@
+﻿using BankLoansManagement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankLoansManagement.Data
+{
+    public class BankLoanContext : DbContext
+    {
+        public BankLoanContext(DbContextOptions<BankLoanContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}

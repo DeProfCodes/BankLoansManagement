c9a2c8b8173fe499317b26e62b0f66a93df9e550
Proficient Mkansi <de.prof.codes@gmail.com>
2022-08-14 21:28:28 +0200

    - add menu bar

--- BankLoansManagement/wwwroot/js/site.js
+++ BankLoansManagement/wwwroot/js/site.js
@@ -1,4 +1,12 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿
+function ToggleMenu()
+{
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav")
+  {
+    x.className += " responsive";
+  } else
+  {
+    x.className = "topnav";
+  }
+}
